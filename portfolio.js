28cee3b0ff07e49bb3f4b15c02c7e97ca639dd22
cliff9509 <kipronowycliff992@gmail.com>
2025-04-03 11:49:30 +0300

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('a[href^="#"]').forEach
+(anchor => {
+    anchor.addEventListener('click',function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute
+            ('href')
+        ).scrollIntoView({
+            behaviour:'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll',() =>{
+    const navbar = document.querySelector('.navbar');
+    window.scrollY > 50 ?
+    navbar.computedStyleMap.background = 'rgba(10,10, 10, 0.98)':
+    navbar.computedStyleMap.background='rgba(10, 10, 10, 0.95)'; 
+    
+});
